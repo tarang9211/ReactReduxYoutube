e5edf0d8e58a8b1c58d77fbffb4519166c6d0517
Tarang Hirani <tarang9211@gmail.com>
2016-11-10 01:44:44 +0530

    wip: displaying default video list item components

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 //Functional component since video_list just displays data
 const VideoList = (props) => {
+  
+  //mapping over videos
+  const VideoItems = props.videos.map((video) => {
+    return <VideoListItem video={ video } key={ video.etag } />
+  });
+
   return (
     <ul className='col-md-4 list-group'>
-      
+      { VideoItems }
     </ul>
   )
 }
