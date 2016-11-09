2c2d28034365794a5fae6dcba233fb5b1afb34c6
Tarang Hirani <tarang9211@gmail.com>
2016-11-10 00:29:05 +0530

    wip: working with states for the search_bar component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    //initializing component state
+    this.state = { searchTerm: '' };
+  }
+
   render() {
-    return <input onChange = { (event) => console.log(event.target.value)} />
+    return (
+      <div>
+        <input
+          value = { this.state.searchTerm }
+          placeholder = 'Search for a video!'
+          onChange = { (e) => this.setState({ searchTerm: e.target.value })} />
+      </div>
+    );
   }
 
 }
