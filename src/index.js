2e4500b524598a409d4486c1097f6aea202b0ead
Tarang Hirani <tarang9211@gmail.com>
2016-11-10 02:02:42 +0530

    wip: loading first video to make sure video_detail works

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBwwj4UuzrSiF3L1ajGGMl-7e5pNgeSyGc';
 
@@ -24,6 +25,7 @@ class App extends Component {
       return (
           <div>
             <SearchBar />
+            <VideoDetail video={this.state.videos[0]}/>
             <VideoList videos= { this.state.videos }/>
           </div>
       );
