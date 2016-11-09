a608d1783ffa33de9236102e724f98802e16644b
Tarang Hirani <taranghirani@Tarangs-MacBook-Pro.local>
2016-11-09 23:22:05 +0530

    wip: added basic component for search_bar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//importing components
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyBwwj4UuzrSiF3L1ajGGMl-7e5pNgeSyGc';
 
 //Step 1: create a component that will produce some HTML
 const App = () => {
-  return <div>Hi!</div>
-}
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
 
 //Step 2: take the component's generated HTMl and put it on the page
 
